7b506bbe8f1fceb88d4c7bccbdcd24b66410c64c
Aleks <erldimitrovgrad@gmail.com>
2024-11-20 11:40:54 +0300

    change style

--- src/main/java/hexlet/code/service/UserService.java
+++ src/main/java/hexlet/code/service/UserService.java
@@ -8,7 +8,6 @@ import hexlet.code.mapper.UserMapper;
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
