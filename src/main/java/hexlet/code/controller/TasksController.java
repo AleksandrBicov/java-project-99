029183488f11a06705c1a5a4a9644132f29cc477
Aleks <erldimitrovgrad@gmail.com>
2024-11-21 13:08:34 +0300

    edit Controllers

--- src/main/java/hexlet/code/controller/TasksController.java
+++ src/main/java/hexlet/code/controller/TasksController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,7 @@ public class TasksController {
     private final TaskService service;
 
     @GetMapping()
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<TaskDTO>> getAll(ParamsDTO params) {
         var tasks = service.getAll(params);
         return ResponseEntity.ok()
@@ -36,24 +38,28 @@ public class TasksController {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.OK)
     public TaskDTO getById(@PathVariable Long id) {
         return service.getById(id);
     }
 
     @PostMapping()
+    @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.CREATED)
     public TaskDTO create(@Valid @RequestBody TaskCreateDTO taskData) {
         return service.create(taskData);
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.OK)
     public TaskDTO update(@RequestBody @Valid TaskUpdateDTO taskData, @PathVariable Long id) {
         return service.update(taskData, id);
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         service.delete(id);
