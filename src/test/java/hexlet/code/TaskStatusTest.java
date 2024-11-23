b6f36e432014da1b2c41700502768be2f4bcf387
Aleks <erldimitrovgrad@gmail.com>
2024-11-23 16:36:12 +0300

    edit TaskStatusTest

--- src/test/java/hexlet/code/TaskStatusTest.java
+++ src/test/java/hexlet/code/TaskStatusTest.java
@@ -22,6 +22,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -42,6 +43,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class TaskStatusTest {
 
     @Autowired
@@ -79,12 +81,6 @@ class TaskStatusTest {
         testTaskStatus = Instancio.of(modelGenerator.getTaskStatusModel()).create();
 
         repository.save(testTaskStatus);
-    }
-
-    @AfterEach
-    @Transactional
-    public void tearDown() {
-        repository.deleteAll();
     }
 
     @Test
